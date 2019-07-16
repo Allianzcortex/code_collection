ec6ee12daac0919005bc7464277ea6f683be5c39
Allianzcortex <iamwanghz@gmail.com>
2019-07-16 12:14:17 -0300

    add new shell file about docker

--- shell-practice/docker_practice.sh
+++ shell-practice/docker_practice.sh
@@ -1,40 +1,110 @@
 #!/bin/bash
 
-# currently part of final codes
+# 1,2,3,6 are exit signals
+trap clean_docker 1 2 3 6
 
-for (( c=2001;c<=80000; c++ ))
+show_progress()
+{   
+    echo "This is task $1 *****************"
+    sleep 2
+}
+
+user="######"
+
+# 1 & 2 remove the folder if it already exists
+show_progress 1and2
+if [ -d "out" ]; then rm -Rf out; fi
+mkdir out && cd out
+
+# 3 get the target file
+show_progress 3
+wget /docker/Dockerfile
+wget /docker/app.py
+
+# 4 get the target string based on date
+show_progress 4
+current_day=$(date '+%d')
+if (( $current_day % 2 == 0 ))
+then
+    new_text='Today is an even day'
+else
+    new_text='Today is an odd day'
+fi
+
+echo "The text get is : $new_text"
+
+# replace 
+sed -i "s/Hello World!/$new_text/g" app.py
+
+# 5 & 6
+show_progress 5and6
+
+docker_image="$user"_a2
+
+# get available port;max port in Linux is 65535
+for (( c=2000;c<=65535; c++ ))
 do
    resp=`netstat -tunl | grep ":$c "`
    if [ -z "$resp" ]; then
-    echo "$c Port is free"
-    avaialble_port=$c
+    echo "$c port is available"
+    available_port=$c
     break
+   else 
+    echo "$c port is already used"
   fi
 done
-echo "avaialble port is $avaialble_port"
 
+# build docker image
+docker build -t $docker_image .
+docker run -d  -p $available_port:80 $docker_image
 
-# 8
-# https://www.shellhacks.com/check-website-availability-linux-command-line/
+# 7
+show_progress 7
 
-    echo "web page is right"
+FS=' ' read -r -a array <<< "$(docker ps | grep $docker_image)"
+container_id=${array[0]}
+container_ip_address=$(docker inspect $container_id | grep "\"IPAddress\"" | head -1 | sed 's/,//g' | cut -d":" -f2 )
+
+echo "\"Container\'s IP is $container_ip_address"
+
+# very important,or docker image may not be built
+sleep 5
+
+# define variables and utils functions used 
+available_url="lnx.cs.smu.ca:"$available_port
+clean_docker()
+{
+    docker stop $container_id
+    docker rm $container_id
+    docker rmi $docker_image
+}
+
+# 8 check https://www.shellhacks.com/check-website-availability-linux-command-line/ for more details
+# 200 is the HTTP status code that represents the function-well page
+show_progress 8
+if [[ $(curl -Is $available_url| head -n 1 | grep 200) ]];then
+    echo "web page works correctly"
 else
-    echo "no webpage"
-    # exit and clean todo
+    echo "web page doesn't work correctly , will remove the docker container and clean it"
+    # exit and clean 
+    clean_docker
+    exit 1
 fi
 
-# 9
+# 9 save webpage 
+show_progress 9
+echo "available url is : "
+echo $available_url
 
-# 10
-# TODO
+wget $available_url -O serv.html
 
-# 11
-
+# 10 
 
 
-# docker stop first
-docker ps | grep your_user_name | awk '{print $1}' | xargs docker stop
+ssh -R $available_port:localhost:$available_port $user@dev.cs.smu.ca "wget -t 1 smu.ca localhost:$available_port" 
 
-docker rm container_name
-docker rmi image_name
+# 11 clean the docker
+show_progress 11
+clean_docker
+
 exit 0
