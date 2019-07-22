b4667f66fda343e9d402859cd2393ea0e8c57cef
Allianzcortex <iamwanghz@gmail.com>
2019-07-22 16:05:05 -0300

    finish SVM and neural network part code

--- Machine-Learning-Code-Base/prediction/prediction.py
+++ Machine-Learning-Code-Base/prediction/prediction.py
@@ -4,220 +4,155 @@ from statsmodels.tsa.arima_model import ARIMA
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 import numpy as np
+from random import randint
 times = []
 values = []
 sum = 0
 index = 0
 
-date = ['01',
-        '02',
-        '03',
-        '04',
-        '05',
-        '06',
-        '07',
-        '08',
-        '09',
-        '10',
-        '11',
-        '12',
-        '13'
-        ]
 
-date = [1,
-        2,
-        3,
-        4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10,
-        11,
-        12,
-        13
-        ]
-value = [19330.143540669856600,
-         30641.148325358849700,
-         23813.397129186604700,
-         23272.727272727275100,
-         22866.028708133973200,
-         23961.722488038278900,
-         25856.459330143542400,
-         29598.086124401913600,
-         30263.157894736844900,
-         24167.464114832536600,
-         23047.846889952151200,
-         23995.215311004792000,
-         23679.425837320575800]
-
-value = [1,
-         3,
-         2,
-         2,
-         2,
-         2,
-         2,
-         2,
-         3,
-         2,
-         2,
-         2,
-         2]
-
-
-with open("MT123electricity.csv") as input_:
+time_list = []
+value_list = []
+with open("ElectricityByDay.csv") as input_:
     for line in input_:
-        index += 1
-        times.append(line.split(",")[0])
-        values.append(line.split(",")[1])
-        sum += float(line.split(",")[1][1:-2].strip("\n"))
-        if(index > 1):
-            break
-
-# print(sum)
-# plt.plot(times, values)
-# plt.show()
+        time_list.append(line.split(",")[0][-2])
+        value_list.append(float(line.split(",")[1][1:-2].strip("\n")))
 
 
-def neural_network_predict():
-    """ analyze by neural network
+def get_svm_index_based_method1(index, time_list, value_list):
+    """ used to get index in method1 mentioned in doc of svm_predict()
     """
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    # X_train =
-    # scaler.fit(X_train)
-    # X_train = scaler.transform(X_train)
-    # X_test = scaler.transform(X_test)
-    # global value
-    # data = date[:-1]
-    # print(data[-1])
-    # data = np.array([data]).T
-    # value = [int(x) for x in value]
-    # label = np.array(value[:-1])
-    global date
-    global value
-    value = list(map(int, value))
-    print(type(value[0]))
-    print(value)
-    date = np.array(date[:-1]).reshape(-1, 1)  # .astype(np.float64)
-    value = np.array(value[:-1])  # .astype(np.float64)  # .reshape(-1, 1)
-    from sklearn.neural_network import MLPRegressor
-    mlp = MLPRegressor(solver='lbfgs', alpha=1e-5,
-                       hidden_layer_sizes=(5, 2), random_state=1)
-    print(date)
-    print(type(value[0]))
-    print(type(date[0]))
+    data = np.array(time_list[index:index+7]).T.reshape(-1, 1)
+    label = np.array(value_list[index:index+7])
+    return (data, label)
 
-    date = [[1, 1],
+
+def get_svm_index_based_method2(index, time_list, value_list):
+    """ used to get index in method2 mentioned in doc of svm_predict()
+    """
+    data = np.array(list(time_list[index+7*i] for i in range(7))).T.reshape(-1, 1)
+    label = np.array(list([value_list[index+7*i] for i in range(7)])).T.reshape(-1, 1)
+    return (data,label)
+
+
+def svm_predict():
+
+    # method 1 : past 7 days                         called month on month
+    # method 2 : the same day in the past 7 weeks   called year on year
+    # method 3 : the same day in the past 7 years   # very limited data , so we will not adopt this method
+    # For method 1
+    # firstly , get 200 records
+    # then get random value
+    expected_result_list1 = []
+    predict_result_list1 = []
+    expected_result_list2 = []
+    predict_result_list2 = []
+    from sklearn.svm import SVR
+    for _ in range(200):
+        index1 = randint(1, 1450)  # hard coded
+        index2 = randint(1, 1412)   # 7*7=49. 1456-50=1412
+        # there will be 6 values
+        data1,label1 = get_svm_index_based_method1(index1,time_list,value_list)
+        data2,label2 = get_svm_index_based_method2(index2,time_list,value_list)
+        # and the same 6 labels
+
+        linear_svr = SVR(kernel='linear')
+        linear_svr.fit(data1, label1)
+        to_predict = np.array(['13']).reshape(-1, 1)
+        print("test result is {}, prediction result is {} ".format(
+            value_list[index1+7], linear_svr.predict(to_predict)[0]))
+        expected_result_list1.append(value_list[index1+7])
+        predict_result_list1.append(linear_svr.predict(to_predict)[0])
+        linear_svr.fit(data2,label2)
+        expected_result_list2.append(label2[-1])
+        predict_result_list2.append(linear_svr.predict(to_predict)[0])
+
+    # draw the plot1 for method1 sampling
+    plt.plot(expected_result_list1)
+    plt.plot(predict_result_list1)
+    plt.ylim(ymin=0)
+
+    plt.show()
+    plt.clf()
+
+    # draw the plot2 for method2 sampling
+    plt.plot(expected_result_list2)
+    plt.plot(predict_result_list2)
+    plt.ylim(ymin=0)
+    plt.show()
+
+
+def get_neural_based_method1(index,time_list,value_list):
+    data = [[1, 1],
             [2, 2],
             [3, 3],
             [4, 4],
             [5, 5],
             [6, 6],
-            [7, 7],
-            [8, 8],
-            [9, 9],
-            [10, 10],
-            [11, 11],
-            [12, 12],
-            [13, 13]]
-   # date = np.array(date).reshape(1, -1)
-    value = [1,
-             3,
-             2,
-             2,
-             2,
-             2,
-             2,
-             2,
-             3,
-             2,
-             2,
-             2,
-             2]
-    value = [19330.143540669856600,
-             30641.148325358849700,
-             23813.397129186604700,
-             23272.727272727275100,
-             22866.028708133973200,
-             23961.722488038278900,
-             25856.459330143542400,
-             29598.086124401913600,
-             30263.157894736844900,
-             24167.464114832536600,
-             23047.846889952151200,
-             23995.215311004792000,
-             23679.425837320575800]
-    # print(date.shape)
-    # value=np.array(value).reshape(1,-1)
-    mlp.fit(date, value)
-    # X = [[0., 0.], [1., 1.]]
-    # y = [0, 1]
-    # mlp.fit(X, y)
-    # print(mlp.predict([[2., 2.]]))
-    predictions = mlp.predict([[13, 13]])
-    print(predictions)
+            [7, 7]]
+    label = np.array(value_list[index:index+7])
+    return (data, label)
+
+def get_neural_based_method2(index,time_list,value_list):
+    data = [[1, 1],
+            [2, 2],
+            [3, 3],
+            [4, 4],
+            [5, 5],
+            [6, 6],
+            [7, 7]]
+    label = np.array(list([value_list[index+7*i] for i in range(7)]))
+    return data,label
+
+def neural_network_predict():
+    """ analyze by neural network
+    """
+
+    expected_result_list1 = []
+    predict_result_list1 = []
+    expected_result_list2 = []
+    predict_result_list2 = []
+
+   
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.neural_network import MLPRegressor
+    scaler = StandardScaler()
 
 
-def svm_predict():
-    # date = ['2011-01-01',
-    #         '2011-01-02',
-    #         '2011-01-03',
-    #         '2011-01-04',
-    #         '2011-01-05',
-    #         '2011-01-06',
-    #         '2011-01-07',
-    #         '2011-01-08',
-    #         '2011-01-09',
-    #         '2011-01-10',
-    #         '2011-01-11',
-    #         '2011-01-12',
-    #         '2011-01-13'
-    #         ]
-    date = [1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            12,
-            13
-            ]
-    value = [19330.143540669856600,
-             30641.148325358849700,
-             23813.397129186604700,
-             23272.727272727275100,
-             22866.028708133973200,
-             23961.722488038278900,
-             25856.459330143542400,
-             29598.086124401913600,
-             30263.157894736844900,
-             24167.464114832536600,
-             23047.846889952151200,
-             23995.215311004792000,
-             23679.425837320575800]
+    for _ in range(200):
+        index1 = randint(1, 1450)  # hard coded
+        index2 = randint(1, 1412)   # 7*7=49. 1456-50=1412
+        data1,label1 = get_neural_based_method1(index1,time_list,value_list)
+        data2,label2 = get_neural_based_method2(index2,time_list,value_list)
+    # data = np.array(time_list).reshape(-1, 1)  # .astype(np.float64)
+    # label = np.array(value_list[:-1])  # .astype(np.float64)  # .reshape(-1, 1)
 
-    from sklearn.svm import SVR
-    import numpy as np
-    data = date[:-1]
-    print(data[-1])
-    data = np.array([data]).T
-    label = np.array(value[:-1])
-    linear_svr = SVR(kernel='linear')
-    linear_svr.fit(data, label)
-    to_predict = np.array(['13']).reshape(-1, 1)
-    print("test result is {}, prediction result is {} ".format(
-        23679.425837320575800, linear_svr.predict(to_predict)))
+        mlp = MLPRegressor(solver='lbfgs', alpha=1e-5,
+                        hidden_layer_sizes=(5, 2), random_state=1)
+        mlp.fit(data1[:-1], label1[:-1])
+        predictions = mlp.predict([[7, 7]])
+        expected_result_list1.append(label1[-1])
+        predict_result_list1.append(predictions[0] if predictions[0]>0 else 0)
+        mlp.fit(data2[:-1],label2[:-1])
+        expected_result_list2.append(label2[-1])
+        predictions = mlp.predict([[7, 7]])
+        predict_result_list2.append(predictions[0] if predictions[0]>0 and predictions[0]<50000 else 0)
+    print(predict_result_list1)
+    plt.plot(expected_result_list1)
+    plt.plot(predict_result_list1)
+    plt.ylim(ymin=0)
+
+    plt.show()
+    plt.clf()
+
+    # draw the plot2 for method2 sampling
+    plt.plot(expected_result_list2)
+    plt.plot(predict_result_list2)
+    plt.ylim(ymin=0)
+    plt.show()
 
 
-def test_series_analysis():
+def time_series_analysis():
 
     from pandas import read_csv
     from pandas import datetime
@@ -271,38 +206,9 @@ def test_series_analysis():
     print(predictions)
 
 
-def time_series_analysis():
-    import numpy as np
-    import pandas as pd
-#   data = pd.read_csv("Electric_Production.csv", index_col=0)
-
-    data = {  # 'time': pd.Series(['2011-01-01', '2011-01-02', '2011-01-03', '2011-01-04', '2011-01-05', '2011-01-06']),
-        'time': pd.Series(['1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0']),
-        'value': pd.Series([19330.143540669856600, 30641.148325358849700, 23813.397129186604700, 23272.727272727275100,
-                            22866.028708133973200, 23961.722488038278900, 25856.459330143542400, 29598.086124401913600])}
-    data = pd.DataFrame(data)
-    data = pd.read_csv("elec.csv")
-
-    data.index = pd.to_datetime(data.index)
-#     from pyramid.arima import auto_arima
-    from statsmodels.tsa.arima_model import ARIMA
-    model = ARIMA(data.astype(float), order=(3, 1, 2))
-    model_fit = model.fit(disp=0)
-    print(model_fit.forecast()[0])
-#     stepwise_model = auto_arima(data, start_p=1, start_q=1,
-#                                 max_p=3, max_q=3, m=12,
-#                                 start_P=0, seasonal=True,
-#                                 d=1, D=1, trace=True,
-#                                 error_action='ignore',
-#                                 suppress_warnings=True,
-#                                 stepwise=True)
-#     stepwise_model.fit(train)
-#     future_forecast = stepwise_model.predict(n_periods=37)
-#     future_forecast
-
 
 if __name__ == '__main__':
-    svm_predict()
-    #     neural_network_predict()
-    #     time_series_analysis()
+    # svm_predict()
+    # neural_network_predict()
+    time_series_analysis()
 #     test_series_analysis()
