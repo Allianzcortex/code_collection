157850eb61b720c574eb7e7f6fe401709f298379
Allianzcortex <iamwanghz@gmail.com>
2023-04-24 08:59:27 -0300

    add missing advent of code day 6

---
+++ advent_of_code/2022/day_6.go
@@ -0,0 +1,29 @@
+package adventofcode
+
+func day6Part1(input string) int {
+	m := map[byte]int{}
+
+	for index := 0; index < len(input); index++ {
+		ch := input[index]
+		if index >= 4 {
+			m[input[index-4]] -= 1
+			if m[input[index-4]] == 0 {
+				delete(m, input[index-4])
+			}
+		}
+
+		if _, exist := m[ch]; !exist {
+			if len(m) == 3 {
+				return index + 1
+			}
+		}
+		m[ch] += 1
+		// for k, v := range m {
+		// 	fmt.Printf("%v:%d,", string(k), v)
+		// }
+		// fmt.Println()
+
+	}
+
+	return -1
+}
