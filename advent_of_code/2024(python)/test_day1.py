2865a4302c03981a04070ba6869663798efd864a
Allianzcortex <iamwanghz@gmail.com>
2024-11-29 18:40:32 -0500

    fails as expected; should fix this time

--- advent_of_code/2024(python)/test_day1.py
+++ advent_of_code/2024(python)/test_day1.py
@@ -5,7 +5,7 @@ from day1 import add
 class TestSample(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2,4), 5)
+        self.assertEqual(add(2,4), 6)
 
 if __name__ == "__main__":
     unittest.main()
