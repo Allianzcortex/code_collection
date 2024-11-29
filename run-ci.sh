f00f51591412415203f817518753270408798c25
Allianzcortex <iamwanghz@gmail.com>
2024-11-29 17:56:18 -0500

    update new CI

--- run-ci.sh
+++ run-ci.sh
@@ -1,1 +1,22 @@
-cd advent_of_code && cd 2021 && go test && cd ../2022 && go test && cd ../2023 && go test
+cd advent_of_code 
+
+# For solutions with go
+for dir in *"(go)"; do
+    if [ -d "$dir" ]; then
+        echo "Entering directory: $dir"
+        cd "$dir" || exit 1
+
+        if go test ./...; then
+            echo "Tests passed in $dir"
+        else
+            echo "Tests failed in $dir"
+        fi
+        
+        # enter to hold folder
+        cd .. 
+
+for dir in *"(python)"; do
+    if [ -d "$dir" ]; then
+        echo "Entering directory: $dir"
+        cd "$dir" || exit 1
+    python -m unittest discover
